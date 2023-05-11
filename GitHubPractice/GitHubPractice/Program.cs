93d9e9130e700181737290db1c825e24d787cc75
AlisonGlenn <118784338+AlisonGlenn@users.noreply.github.com>
2023-05-10 18:43:26 -0600

    Loop

--- GitHubPractice/GitHubPractice/Program.cs
+++ GitHubPractice/GitHubPractice/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Alison, the wonderful world of GitHub!");
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Iteration number {i}");
+}
