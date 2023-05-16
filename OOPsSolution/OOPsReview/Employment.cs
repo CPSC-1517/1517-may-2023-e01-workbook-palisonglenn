b2dba45636d3b8893d22adaa01f54a953a9cc7b2
AlisonGlenn <118784338+AlisonGlenn@users.noreply.github.com>
2023-05-15 18:08:10 -0600

    Complete properties review

---
+++ OOPsSolution/OOPsReview/Employment.cs
@@ -0,0 +1,24 @@
+﻿namespace OOPsReview
+{
+    public class Employment
+    {
+        // Private members for the class
+        private SupervisoryLevel _Level;
+        private string _Title;
+        private double _Years;
+
+        // Public properties for the class
+        public string Title
+        {
+            get { return _Title; }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Title is required");
+                }
+                _Title = value;
+            }
+        }
+    }
+}
