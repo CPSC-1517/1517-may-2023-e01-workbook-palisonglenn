ec159392e9ecaca38befcd47e01dc941889bc158
AlisonGlenn <118784338+AlisonGlenn@users.noreply.github.com>
2023-05-17 17:18:27 -0600

    Summary

--- OOPsSolution/OOPsReview/Employment.cs
+++ OOPsSolution/OOPsReview/Employment.cs
@@ -20,5 +20,39 @@
                 _Title = value;
             }
         }
+
+        public double Years
+        {
+            get { return _Years; }
+            set
+            {
+                if (!Utilities.IsZeroOrPositive(value))
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _Years = value;
+            }
+        }
+
+        public DateTime StartDate { get; private set; }
+
+        public Employment()
+        {
+            Title = "Unknown";
+            Level = SupervisoryLevel.TeamMember;
+            StartDate = DateTime.Today;
+        }
+
+        public Employment(string title, SupervisoryLevel level, DateTime startdate, double years = 0.0)
+        {
+            Title = title;
+            Level = level;
+            Years = years;  
+            if (startdate >= DateTime.Today.AddDays(1)) 
+            {
+                throw new ArgumentException($"The start date {startdate} is in the future!");
+            }
+            StartDate = startdate;
+        }
     }
 }
--- OOPsSolution/OOPsReview/Utilities.cs
+++ OOPsSolution/OOPsReview/Utilities.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace OOPsReview
 {
-    internal class Utilities
+    public static class Utilities
     {
+        // Static classes are not instantiated by the user
+        // Static class items are referenced using classname.xxx
+        // Methods within the class have the keyword static in their signature
+        // Static classes are shared between all outside users at the same time
+        // DO NOT store data within a static class
+        // Place generic reusable methods with a static class
+
+        // Example: 
+        public static bool IsZeroOrPositive(double value)
+        {
+            bool valid = true;
+            if (value < 0.0)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+        public static bool IsZeroOrPositive(int value)
+        {
+            bool valid = true;
+            if (value < 0)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+
+        public static bool IsZeroOrPositive(decimal value)
+        {
+            bool valid = true;
+            if (value < 0.0m)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+
+
     }
 }
