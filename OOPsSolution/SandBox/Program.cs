3d7b4f91ef706e444e90a8e7335dc7add30921af
AlisonGlenn <118784338+AlisonGlenn@users.noreply.github.com>
2023-05-24 17:46:11 -0600

    Introduction to the record datatype

--- OOPsSolution/SandBox/Program.cs
+++ OOPsSolution/SandBox/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OOPsReview;
+
 Console.WriteLine("Hello, World!");
+
+Residence myHome = new Residence(123, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome.ToString());
+
+// You cannot change a value in the record instance
+// To alter a value in the record instance you must create a new instance
+
+myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome.ToString());
+
