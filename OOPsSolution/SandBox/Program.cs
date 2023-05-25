6206793fa5f60cd218df7b6cb1ae9ae6aea08839
AlisonGlenn <118784338+AlisonGlenn@users.noreply.github.com>
2023-05-24 18:20:57 -0600

    Example of refactoring

--- OOPsSolution/SandBox/Program.cs
+++ OOPsSolution/SandBox/Program.cs
@@ -7,8 +7,56 @@ Residence myHome = new Residence(123, "Maple St.", "Edmonton", "AB", "T6Y7U8");
 Console.WriteLine(myHome.ToString());
 
 // You cannot change a value in the record instance
-// To alter a value in the record instance you must create a new instance
+// myHome.Number = 321;
 
+// To alter a value in the record instance you must create a new instance
 myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
 Console.WriteLine(myHome.ToString());
 
+// Example of refactoring
+// Refactoring is the process of restructuring code while not changing its original functionality. The goal of refactoring is to improve internal code by making small changes without altering the code's external behaviour
+
+// Original code
+bool flag = false;
+if (myHome.Province.ToLower()==("ab"))
+{
+    flag = true;
+}
+
+if (myHome.Province.ToLower()==("bc"))
+{
+    flag = true;
+}
+
+if (myHome.Province.ToLower()==("sk"))
+{
+    flag = true;
+}
+
+if (myHome.Province.ToLower()==("mn"))
+{
+    flag = true;
+}
+
+if (myHome.Province.ToLower() == "ab" || myHome.Province.ToLower() == "bc" || myHome.Province.ToLower() == "sk" ||myHome.Province.ToLower() == "mn")
+{
+    flag = true;
+}
+
+// Refactor using a switch statement
+switch (myHome.Province.ToLower())
+{
+    case "ab":
+    case "bc":
+    case "sk":
+    case "mn":
+        {
+            flag = true;
+            break;
+        }
+    default:
+        {
+            flag = false;
+            break;
+        }
+}
